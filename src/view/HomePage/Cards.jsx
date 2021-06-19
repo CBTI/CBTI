a93d7f68ae0f67ee364076f44b273e64b1b0fbcd
unknown <kthrkdals@gmail.com>
2021-06-19 22:32:53 +0900

    modified by Tommy [Cards.jsx, Cards.scsss, Cards.css, data.jsx] use able Mobile, position change to center, add var. testType, link for test

--- src/view/HomePage/Cards.jsx
+++ src/view/HomePage/Cards.jsx
@@ -2,7 +2,19 @@ import React, {useState, useEffect} from 'react'
 import './Cards.css'
 import {data} from './data'
 // 이 페이지는 왠만하면 state를 건드리지 않는 편이 좋다. retrun 안에 코드들이 무겁기 때문이다.
-const Cards = () => {
+
+
+function checkMobileDevice() {
+    var mobileKeyWords = new Array('Android', 'iPhone', 'iPod', 'BlackBerry', 'Windows CE', 'SAMSUNG', 'LG', 'MOT', 'SonyEricsson');
+    for (var info in mobileKeyWords) {
+        if (window.navigator.userAgent.match(mobileKeyWords[info]) != null) {
+            return true;
+        }
+    }
+    return false;
+}
+
+const Cards = () =>  {
     const [CardObjects, setCardObjects] = useState([]);
 
     useEffect(() => {       /// 심리테스트 데이터를 받아와서 CardObjects state에 넣는다.
@@ -13,31 +25,55 @@ const Cards = () => {
         })
         setCardObjects(items);
     }, [])
-        
-    return (
-        <section className = "container">
-            <div className = "items"> 
-                {CardObjects.map((v,i) =>{
-                    return <div key={v.title + v.author}className="item">
-                        <img src={v.imgsrc} alt={v.title} title={v.title}/>
-                        <div className="item__data">
-                            <h3 className="item__title">{v.title} 심리테스트</h3>
-                            <p className="item__genre">{v.author}</p>
-                            <p className="item__summary">{v.summary}</p>
-                            <a href="#" className="item__link">심리테스트 해보기</a>
-                            <h5 className="item__year">출시일 : {v.years}</h5>
-                        </div>
-                    </div>
-                    }
-                )
-                }
-            </div>
-        </section>
-    )
 
-    
-    
+    if(checkMobileDevice()) {
+        return (
+            <section className = "container">
+                <div className = "items"> 
+                    {CardObjects.map((v,i) =>{
+                        return <div key={v.title + v.author}className="item">
+                                <img src={v.imgsrc} alt={v.title} title={v.title}/>
+                                <h3 className="item__title">{v.title} {v.testType}</h3>
+                                <div className="btnContainer">
+                                    <div className="btn">
+                                        <div className="btn-eff"></div>
+                                        <a onClick={() => location.href = v.link}>시작하기</a>
+                                    </div>
+                                </div>
+                            </div>
+                        })
+                    }
+                </div>
+            </section>
+        )
+    } else {
+        return (
+            <section className = "container">
+                <div className = "items"> 
+                    {CardObjects.map((v,i) =>{
+                        return <div key={v.title + v.author}className="item">
+                            <img src={v.imgsrc} alt={v.title} title={v.title}/>
+                            <h3 className="item__title">{v.title} 심리테스트</h3>
+                                <p className="item__genre">{v.author}</p>
+                                <p className="item__summary">{v.summary}</p>
+                                <div className="btnContainer">
+                                    <div className="btn">
+                                        <div className="btn-eff"></div>
+                                        <a onClick={() => location.href = v.link}>시작하기</a>
+                                    </div>
+                                </div>
+                                <h5 className="item__year">출시일 : {v.years}</h5>
+                        </div>
+                        }
+                    )
+                    }
+                </div>
+            </section>
+        )
+    }
 }
+
+
 
 export default Cards;
 
