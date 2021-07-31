97d06c1fd83356b9470292a47f66ea3eb5410810
unknown <kthrkdals@gmail.com>
2021-07-31 16:59:48 +0900

    Add

--- src/Component/BackButton.jsx
+++ src/Component/BackButton.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
-import { Button } from 'antd';
-import { LeftCircleOutlined } from '@ant-design/icons';
+import React from "react";
+import { Button } from "antd";
+import { LeftCircleOutlined } from "@ant-design/icons";
 
 const BackButton = (props) => {
-    return <Button type="primary" icon={<LeftCircleOutlined /> } onClick={() => props.history.goBack()} size="large">뒤로가기</Button>
-}
+  return (
+    <Button
+      type="primary"
+      icon={<LeftCircleOutlined />}
+      onClick={() => props.history.goBack()}
+      size="large"
+    >
+      뒤로가기
+    </Button>
+  );
+};
 
-export default BackButton
+export default BackButton;
